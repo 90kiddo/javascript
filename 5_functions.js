aa0fed61b57204e83213de71ce6ec9629d692c24
unknown <gaurav.smec@gmail.com>
2026-02-05 17:36:38 +0530

    added

---
+++ 5_functions.js
@@ -0,0 +1,40 @@
+//function functionName(parameters){
+// function is a block of reusable code
+//}
+
+// function sum(a, b){
+//    return (a + b);
+// }
+
+// console.log(sum(2, 3))
+
+// function greet(name){
+//     console.log('hello ' + name + ' welcome')
+//     console.log(`hello ${name} welcome`)
+// }
+
+// greet('rahul')
+
+// var firstName = function (name){
+//     console.log('hello ' + name + ' welcome')
+//     console.log(`hello ${name} welcome`)
+// }
+
+// firstName('ajay')
+
+
+// IIFE
+
+// var result = (function (a, b){
+//     console.log(a + b)
+//     return(a + b)
+// })(5, 6)
+
+// console.log(result)
+
+
+
+var cal = (a, b) => {
+      
+}
+
---
+++ ecma.js
@@ -0,0 +1,15 @@
+// let, const
+
+// template strings ``
+
+// normal function
+
+// const sum = function(a, b){
+//     return (a + b)
+// }
+
+// arrow function
+
+// const sum = (a, b) => {
+//     return (a + b)
+// }
