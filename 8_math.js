19d1b11e182dd2f886ef9c03537e0d673b00c424
unknown <gaurav.smec@gmail.com>
2026-02-08 13:48:25 +0530

    added

---
+++ 8_math.js
@@ -0,0 +1,22 @@
+//Match.round()  rounds the nearest integer
+//Match.floor()  rounds down the nearest integer
+//Match.ceil()  rounds up the nearest integer
+//Math.trunc()  returns the integer part
+
+
+//Math.pow(x, y)  returns the value of x to thr power of y
+
+// console.log(Math.pow(2, 3))
+// console.log(2 ** 3)
+
+
+// Math.sqrt()
+
+// Math.log(x)   returns the natural logarithm of x
+
+// console.log(Math.log(3))
+// console.log(Math.log2(3))
+
+// Math.random()  returns a random number between 0(incusive) and 1(exclusive)
+
+// console.log(Math.random())
---
+++ 9_dom.js
@@ -0,0 +1,34 @@
+// Window (global object)
+
+// DOM  child of a window object (tree of objects)
+// DOM provides a way to interact with HTML document using JavaScript 
+
+// Window - DOM, BOM, JavaScript
+
+//document properties
+//getElementById()
+//getElementByClassName()
+//getElementByTagName()
+//querySelector()
+//querySelectorAll()
+//innerHTML
+//textContent
+//style
+
+
+//document methods
+//createElement(tagName)
+//appendChild(node)
+//removeChild(node)
+//addEventListener(event, function)
+//removeEventListener(event, function)
+//setAttribute(name, value)
+//getAttribute(name)
+//parentNode / parentElement
+//childNodes / children
+//firstChild / firstElementChild
+//lastChild / lastElementChild
+//nextSibling / nextElementSibling
+//previousSibling / previousElementSibling
+//closest(selector)
+//forEach(Array.from)
